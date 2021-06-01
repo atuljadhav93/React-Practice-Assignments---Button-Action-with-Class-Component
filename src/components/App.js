7f5e297198440ca97ac8a8890f59190b458d1bd0
Atul <atuljadhav618@gmail.com>
2021-06-01 18:30:09 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    constructor(props) {
+class App extends Component 
+{
+    constructor(props) 
+	{
 		super(props);
-	};
-
-    render() {
+		this.state = {isClicked:false};
+		// this.printPara=this.printPara.bind(this);
+	};	
+    render() 
+	{
+		const printPara =()=>{
+			// alert("here");
+			this.setState({isClicked:true});
+		}
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={printPara}>
+					Click Me
+				</button>
+				{this.state.isClicked ?(
+					<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+				):null}
     		</div>
     	);
     }
@@ -17,4 +31,3 @@ class App extends Component {
 
 
 export default App;
-
